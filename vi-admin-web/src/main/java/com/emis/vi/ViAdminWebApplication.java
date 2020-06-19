8babb88af42bcb07731c019210a2a45ea7302bb3
francis-xie <1159394241@qq.com>
2020-06-19 16:34:05 +0800

    2020-06-19 调试

---
+++ vi-admin-web/src/main/java/com/emis/vi/ViAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.emis.vi.viadminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViAdminWebApplication.class, args);
+	}
+
+}
---
+++ vi-admin-web/src/test/java/com/emis/vi/ViAdminWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emis.vi.viadminweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViAdminWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
